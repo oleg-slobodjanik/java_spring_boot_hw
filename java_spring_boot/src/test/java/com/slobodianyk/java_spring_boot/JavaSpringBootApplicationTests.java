74fea7c9b091c4ce1ec39fed7f786d083dd5b2d5
oleg <slobodyanik715@gmail.com>
2024-10-24 15:29:06 +0300

    create spring boot project

---
+++ java_spring_boot/src/test/java/com/slobodianyk/java_spring_boot/JavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slobodianyk.java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
